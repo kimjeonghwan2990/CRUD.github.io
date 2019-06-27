575cae427fad458e03560edd3e1bf93eb79144ad
kimjenoghwan2991 <vmfosemwjd@likelion.org>
2019-06-27 20:58:42 +0900

    crud

--- funccrud/urls.py
+++ funccrud/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
 
-    path('', views.read, name="home"),
-    path('newblog/', views.create, name="newblog"),
-    path('update/<int:pk>', views.update, name="update"),
-    path('delete/<int:pk>', views.update, name="delete"),
-
+path('', views.read, name="home"),
+path('newblog/', views.create, name="newblog"),
+path('update/<int:pk>', views.update, name="update"),
+path('delete/<int:pk>', views.delete, name="delete"),
+path('comment/<int:pk>', views.add_comment, name='add_comment'),
 ]
